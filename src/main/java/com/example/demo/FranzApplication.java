458ac4d72a52ac928fc4ddb5600570ff4d5358ac
Franz Quispe <114706673+Franz04Quispe@users.noreply.github.com>
2022-12-13 10:41:59 -0400

    Add files via upload
    Pregunta 9 del examen final

---
+++ src/main/java/com/example/demo/FranzApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranzApplication.class, args);
+	}
+
+}
